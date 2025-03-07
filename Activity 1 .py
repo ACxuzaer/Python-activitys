c5b290ebe5427830dbd9352c0065f60d6cdd1d1e
ACxuzaer <uzaer2011@gmail.com>
2025-03-07 22:31:28 +0600

    Add files via upload

---
+++ Activity 1 .py
@@ -0,0 +1,7 @@
+print("hello my name is abdullah")
+
+print (8)
+
+print("Today we will learn Python\n in codingal we will learn some new things")
+
+print("Hello my name is Abdullah uzaer" ,87,23,98,78)
