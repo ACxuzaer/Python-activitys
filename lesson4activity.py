b0b8c28476026daa78342d0fec6f5167c57c66ed
ACxuzaer <uzaer2011@gmail.com>
2025-03-12 22:25:39 +0600

    Add files via upload

---
+++ lesson4activity.py
@@ -0,0 +1,14 @@
+#storing values
+tree1=70
+tree2=56
+tree3=67
+tree4=30
+tree5=11
+
+#finiding the total of the sum
+sum=(tree1+tree2+tree3+tree4+tree5)
+print ("the sum of all the 5 trees is:", sum)
+
+#finding the avarage of the sum
+avarage=(sum/5)
+print ("the avarage of the tree is :", avarage)
