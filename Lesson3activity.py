45bfe4c204dd56f29ddd06475eb5b2fac5a306dd
ACxuzaer <uzaer2011@gmail.com>
2025-03-12 19:32:23 +0600

    Add files via upload

---
+++ Lesson3activity.py
@@ -0,0 +1,11 @@
+print("hello my name is abdullah uzaer and today i will talk about some thing")
+print("This is the only thing that dont hate very much")
+a = 908761237465098
+b = "power of justice"
+c =  True 
+d = 0.45
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
