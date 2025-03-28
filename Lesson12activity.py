6920dc5f8c3bf78da01af1790862e08542df4eeb
ACxuzaer <uzaer2011@gmail.com>
2025-03-28 22:31:54 +0600

    Add files via upload

---
+++ Lesson12activity.py
@@ -0,0 +1,8 @@
+Medical_cause=input("did you have an medical cause Y or N")
+"if medical_cause == 'Y':
+    print ("You are allowed")
+else:
+    if atten>=75: 
+        print("Alowed")
+    else:
+        print("not allowed")
