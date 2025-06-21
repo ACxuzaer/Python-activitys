365cb3f95789669bf545788b20d27b41e4fadd3a
ACxuzaer <uzaer2011@gmail.com>
2025-06-21 15:00:29 +0600

    Add files via upload

---
+++ Lesson50activity.py
@@ -0,0 +1,26 @@
+# Import Necessary Libraries
+import pygame 
+
+# Initialize required modules
+pygame.init()
+
+
+# Setup window geometry
+screen = pygame.display.set_mode((400,500))
+
+
+# Create a loop tp run till the game is quilt by the user
+done = False
+
+
+while not done:
+
+
+    # Clear the event queue
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                  pygame.quit()
+
+
+    # Make the changes visible
+    pygame.display.flip()
