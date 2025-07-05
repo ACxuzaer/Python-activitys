fc4ec8a8a5a0d2465e7e70ae2de6626478273153
ACxuzaer <uzaer2011@gmail.com>
2025-07-05 14:41:35 +0600

    Add files via upload

---
+++ Lesson59activity.py
@@ -0,0 +1,12 @@
+# Import necessary libraries
+from tkinter import *
+
+# Create Window
+window = Tk()
+
+# Set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('400x300')
+
+# Start the window's event loop
+window.mainloop()
