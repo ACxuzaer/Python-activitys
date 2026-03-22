f121ae1f6a1f10c0ec055373ec0aed423d88275e
ACxuzaer <uzaer2011@gmail.com>
2026-03-22 18:22:25 +0600

    Add files via upload

---
+++ Self Development in codes-----With Project---1.py
@@ -0,0 +1,19 @@
+a = float(input("Enter 1st number: "))
+b = float(input("Enter 2nd number: "))
+
+op = (input("Choose +,-,*, /:"))
+
+if op == "+":
+   print(a + b)
+
+elif op == "-":
+   print(a - b)
+
+elif op == "*":
+   print(a * b)
+
+elif op == "/":
+   print(a / b)
+
+else:
+   print("Your brain is not properly grown Yet.......")
